359a14787306ba5ae85ea4c36970d213be00094f
alexeykaratay <karatayalexey@gmail.com>
2023-10-16 00:12:40 +0300

    refactor Singleton.java

--- src/main/java/com/yahoo/search/singleton/Singleton.java
+++ src/main/java/com/yahoo/search/singleton/Singleton.java
@@ -1,4 +1,25 @@
 package com.yahoo.search.singleton;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Singleton {
+    private static WebDriver driver;
+    private Singleton() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
